5fa7081aa981a089f330285f6e1f158c543aa819
seunghyun-24 <hyunlight@korea.ac.kr>
2025-04-03 03:03:56 +0900

    3.2 sec 설명

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const express = require("express");
-const app = express();
+
 const port = 3000;
-app.use(express.urlencoded({ extended: true }));
-const objectToString = (obj) =>
-  Object.keys(obj)
-    .map((k) => `${k}: ${obj[k]}`)
-    .join("\n");
-app.get("/", (req, res) => res.send(objectToString(req.query)));
-app.post("/", (req, res) => res.send(objectToString(req.body)));
-app.put("/", (req, res) => res.send(objectToString(req.body)));
-app.delete("/", (req, res) => res.send(objectToString(req.body)));
+const app = express();
+
+app.set("views", `${__dirname}/views`);
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => res.render("index.pug"));
+
+app.get("/page", (req, res) => {
+  const { page } = req.query;
+  res.render("board.pug", { page });
+});
+
+app.get("/posts", (req, res) => {
+  const { until } = req.query;
+  const untilParsed = parseInt(until, 10);
+  const posts = [];
+  if (!isNaN(untilParsed)) {
+    for (let i = 0; i < untilParsed; i++) {
+      posts.push(`Post ${i + 1}`);
+    }
+  }
+  res.render("posts.pug", { posts });
+});
+
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
